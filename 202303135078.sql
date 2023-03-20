174ba66fb2b4d773f6c74217e4f4c29bd3880e73
xiaowei12386 <57929677+xiaowei12386@users.noreply.github.com>
2023-03-20 13:35:07 +0800

    Add files via upload

---
+++ 202303135078.sql
@@ -0,0 +1,3 @@
+
+insert into tsys_subtrans (TRANS_CODE, SUB_TRANS_CODE, SUB_TRANS_NAME, REL_SERV, REL_URL, CTRL_FLAG, LOGIN_FLAG, REMARK, EXT_FIELD_1, EXT_FIELD_2, EXT_FIELD_3)
+values ('ifmCDyglServiceFeeByInstType', 'ifmCDyglServiceFeeByInstTypeHz', '服务费汇总报表按机构子类子类型汇总', 'bank.hsbc.fui.syncaudit.fund.query.servicefeeByInstTypeService', '', '0', '1', '', '00', '', '');
